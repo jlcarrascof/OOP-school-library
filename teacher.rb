7c02b37dfd2e6862fbb4849b04f197d45c4d7e25
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-03 00:10:09 -0400

    Fix teacher.rb class to add parent permission

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, name = 'Unknown', age = nil)
-    super(name, age)
+  def initialize(specialization,  age = nil, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
