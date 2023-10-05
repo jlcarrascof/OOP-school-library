ad425f716a16ef8fa1f0efea2e286ab1b8856b1d
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-04 21:13:40 -0400

    Create rental.rb class

---
+++ rental.rb
@@ -0,0 +1,11 @@
+class Rental
+  attr_reader :book, :person, :date
+  
+  def initialize(book, person, date)
+    @book = book
+    @book.rentals.push(self) unless @book.rentals.include?(self)
+    @person = person
+    @person.rentals.push(self) unless @person.rentals.include?(self)
+    @date = date
+  end
+end
