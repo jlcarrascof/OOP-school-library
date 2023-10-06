2d9d2f40c0c0d3c532f6eaf37cb0bc4c15680be6
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-06 02:26:53 -0400

    Revert to my original rental class like in the previous branch

--- rental.rb
+++ rental.rb
@@ -1,12 +1,11 @@
 class Rental
-  attr_accessor :date, :book, :person
+  attr_reader :book, :person, :date
 
-  def initialize(date, book, person)
-    @date = date
+  def initialize(book, person, date)
     @book = book
+    @book.rentals.push(self) unless @book.rentals.include?(self)
     @person = person
-
-    book.rentals << self
-    person.rentals << self
+    @person.rentals.push(self) unless @person.rentals.include?(self)
+    @date = date
   end
 end
